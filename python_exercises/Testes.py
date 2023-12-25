fb06d2bf2d79c7623e185e6c6f4ebffed697297c
mateusfl1998 <154616064+mateusfl1998@users.noreply.github.com>
2023-12-25 10:05:34 -0300

    Update Testes.py

--- python_exercises/Testes.py
+++ python_exercises/Testes.py
@@ -1,13 +1,11 @@
-def remove_adjacent(nums):
-    resultado = []
-    if len(nums) > 1:
-        resultado = [nums[0]]
-    for num in nums:
-        if num != resultado[-1]:
-            resultado.append(num)
-    
-    print()
-    print(resultado)
-            
-
-remove_adjacent([])
+def sort_last(tuples):
+    nova_lista = []
+    for conjunto in tuples:
+        conjunto = list(conjunto)
+        x0 = conjunto[0]
+        x1 = conjunto[1]
+        novo_conjunto = x1,x0
+        novo_conjunto = tuple(novo_conjunto)
+        nova_lista.append(novo_conjunto)
+    print(nova_lista)
+sort_last([(1, 3), (3, 2), (2, 1)])
